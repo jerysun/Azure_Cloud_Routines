e6084134b292c61c3576561c1bbc8a8f8e61ef28
Jerry Sun <jerysun007@hotmail.com>
2021-01-19 23:57:50 +0100

    Added project ABlobStorage

---
+++ ABlobStorage/Models/UploadFileRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ABlobStorage.Models
+{
+    public class UploadFileRequest
+    {
+        public string FilePath { get; set; } //full path in local hard drive
+        public string FileName { get; set; } // file name in the Azure Blob Storage
+    }
+}
---
+++ ABlobStorage/Services/IBlobService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ABlobStorage.Services
+{
+    public interface IBlobService
+    {
+        Task<BlobInfo> GetBlobAsync(string blobName);
+
+        Task<IEnumerable<string>> ListBlobsAsync();
+
+        Task UploadFileBlobAsync(string filePath, string fileName);
+
+        Task UploadContentBlobAsync(string content, string fileName);
+
+        Task DeleteBlobAsync(string blobName);
+    }
+}
